14dc9c9dd52ef842d9b996f787af87b9b9c41f1d
KauaGit0 <kaualaurentiino@gmail.com>
2026-03-05 18:16:28 -0300

    Update Aula.py

--- Aula Python/Aula.py
+++ Aula Python/Aula.py
@@ -1,2 +1,15 @@
-import math, sys
-print(math.sin(math.pi/2))
+from math import radians, pi, degrees, sin, asin, cos, tan
+from random import random as teste
+
+ad = 90
+ar = radians(ad)
+ad = degrees(ar)
+
+print(ad == 90)
+print(ar == pi / 2)
+print(sin(ar) / cos(ar) == tan(ar))
+print(asin(sin(ar)) == ar)
+
+for i in range(5):
+    print(teste())
+
