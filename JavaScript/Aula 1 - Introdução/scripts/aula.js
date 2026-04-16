aad45ac4cf6557092c47bff839f40dbb64e29b03
KauaGit0 <kaualaurentiino@gmail.com>
2026-04-16 15:50:19 -0300

    js

---
+++ JavaScript/Aula 1 - Introdução/scripts/aula.js
@@ -0,0 +1,36 @@
+//  Resposta no console
+console.log("Olá Mundo!");
+
+//   Caixas de dialogo
+// alert("Está funcionando aí.");
+// prompt("Qual é o seu nome?");
+// confirm("Você faria um pix para seu colega? ");
+
+//  acessa documento, busca o elemento com id selecionado
+//  inner = dentro
+
+// document.getElementById("resposta").innerText = "QueLegal";
+
+document.getElementById("verdade").innerHTML = "<h1>Saitama > Goku</h1>";
+
+document.getElementById("resposta").innerHTML = "<h1>Atack on Titan > todos animes</h1>";
+
+// string
+var nome = "Kaua";
+
+// boolean
+var ligado = true;
+
+// float
+var dinheiro = 150.90;
+
+// number
+var idade = 23;
+// array - lista, vetor
+var listasCompras = []
+
+console.log(typeof(nome));
+console.log(typeof(ligado));
+console.log(typeof(idade));
+console.log(typeof(dinheiro));
+console.log(typeof(listasCompras));
