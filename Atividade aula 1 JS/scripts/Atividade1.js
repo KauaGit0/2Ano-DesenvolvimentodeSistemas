e2433457a52f3b95fcfe4fa167dcb696e8abf426
KauaGit0 <kaualaurentiino@gmail.com>
2026-04-16 17:21:55 -0300

    Atividade JS Final
    Alteração final correta

--- Atividade aula 1 JS/scripts/Atividade1.js
+++ Atividade aula 1 JS/scripts/Atividade1.js
@@ -3,4 +3,4 @@ var conta = Number(prompt("Digite o valor da conta:"));
 var clientes = Number(prompt("Quantos pessoas tem na mesa?"));
 
 
-document.getElementById("resposta1").innerHTML = `<h1>O total deu R$${conta} e para cada pessoa ficará R$${conta / clientes} com ${clientes} clientes</h1>`;
+document.getElementById("resposta1").innerHTML = `<h1>O total deu R$${conta}. Possuindo ${clientes} pessoas, para cada uma ficará R$${conta / clientes}.</h1>`;
